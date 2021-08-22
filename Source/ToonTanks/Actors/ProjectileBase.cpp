655128572f17e1c74d2e4029c1fdf91924b8710e
andresmonc <jaimeamonc@gmail.com>
2021-08-21 22:25:27 -0500

    format added apply damage

--- Source/ToonTanks/Actors/ProjectileBase.cpp
+++ Source/ToonTanks/Actors/ProjectileBase.cpp
@@ -1,30 +1,45 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "ProjectileBase.h"
-#include "GameFramework/ProjectileMovementComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
 
 // Sets default values
-AProjectileBase::AProjectileBase()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+AProjectileBase::AProjectileBase() {
+  // Set this actor to call Tick() every frame.  You can turn this off to
+  // improve performance if you don't need it.
+  PrimaryActorTick.bCanEverTick = false;
 
-	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
-	RootComponent = ProjectileMesh;
-	
-	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
+  ProjectileMesh =
+      CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+  ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectileBase::OnHit);
+  RootComponent = ProjectileMesh;
 
+  ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(
+      TEXT("Projectile Movement"));
 }
 
 // Called when the game starts or when spawned
-void AProjectileBase::BeginPlay()
-{
-	Super::BeginPlay();
-	ProjectileMovement->InitialSpeed = MovementSpeed;
-	ProjectileMovement->MaxSpeed = MovementSpeed;
-	InitialLifeSpan = 3.0f;
+void AProjectileBase::BeginPlay() {
+  Super::BeginPlay();
+  ProjectileMovement->InitialSpeed = MovementSpeed;
+  ProjectileMovement->MaxSpeed = MovementSpeed;
+  InitialLifeSpan = 3.0f;
 }
 
+void AProjectileBase::OnHit(UPrimitiveComponent *HitComp, AActor *OtherActor,
+                            UPrimitiveComponent *OtherComp,
+                            FVector NormalImpulse, const FHitResult &Hit) {
+  AActor *MyOwner = GetOwner();
+  if (!MyOwner) {
+    return;
+  }
 
+  if (OtherActor && OtherActor != this && OtherActor != MyOwner) {
+    UGameplayStatics::ApplyDamage(OtherActor, Damage,
+                                  MyOwner->GetInstigatorController(), this,
+                                  DamageType)
+  }
+}
